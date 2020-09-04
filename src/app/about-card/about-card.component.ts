6bf18de197c8e3880cb665c173ba19b2e6d8a4ef
nosslsvec8 <nosiljevets@gmail.com>
2020-09-04 14:38:08 +0300

    added detailed cards description pages

--- src/app/about-card/about-card.component.ts
+++ src/app/about-card/about-card.component.ts
@@ -1,22 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Cards} from 'src/app/Cards';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about-card',
   templateUrl: './about-card.component.html',
   styleUrls: ['./about-card.component.scss']
 })
-export class AboutCardComponent {
+export class AboutCardComponent implements OnInit {
   cards = new Cards();
   card;
   idCard;
   nameCard;
+  pathname;
 
-  constructor() {
+  constructor(private title: Title) {
     this.idCard = String(window.location).substr(-2).replace(/\//i, '');
-    // console.log('id: ' + this.idCard);
-
+    this.pathname = window.location.pathname;
     this.card = this.cards.cards[this.idCard - 1];
     this.nameCard = this.cards.cards[this.idCard - 1].name;
+  }
+
+  ngOnInit() {
+    this.title.setTitle(this.nameCard);
   }
 }
