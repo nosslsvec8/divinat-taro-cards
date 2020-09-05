4cabdd99a7cf7d6557c6ad12e5e327aac18dc85b
nosslsvec8 <nosiljevets@gmail.com>
2020-09-05 17:41:09 +0300

    removed data output for developer mode

--- src/app/block-info-card/block-info-card.component.ts
+++ src/app/block-info-card/block-info-card.component.ts
@@ -1,20 +1,13 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-block-info-card',
   templateUrl: './block-info-card.component.html',
   styleUrls: ['./block-info-card.component.scss']
 })
-export class BlockInfoCardComponent implements OnInit{
+export class BlockInfoCardComponent {
   @Input() card;
   @Input() idHtml;
   @Input() titleContext;
   @Input() h2Text;
-
-  ngOnInit() {
-    console.log(this.card);
-    console.log(this.idHtml.value);
-    console.log(this.titleContext.value);
-    console.log(this.h2Text);
-  }
 }
