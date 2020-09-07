79c3cfe719d31e64f670eb27e8d8f824033e44b4
nosslsvec8 <nosiljevets@gmail.com>
2020-09-07 10:59:49 +0300

    added menu navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Cards} from 'src/app/Cards';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  cards = new Cards();
+  card;
+  idCard;
+  nameCard = '';
+  pathname;
+
+  constructor() {
+    this.idCard = String(window.location).substr(-2).replace(/\//i, '');
+    this.pathname = window.location.pathname;
+
+    if (this.pathname !== '/home') {
+      this.card = this.cards.cards[this.idCard - 1];
+      this.nameCard = this.cards.cards[this.idCard - 1].name;
+    }
+  }
+}
